42ca2cdfa4368e1e66b1d590371354f7b324d03b
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2023-08-02 17:55:35 +0200

    publish ml process

--- dist/_nuxt/static/1690990381/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1690990381",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"adcf68d7"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null,"\u002F"));
