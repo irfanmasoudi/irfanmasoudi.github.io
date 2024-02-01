9156b2ecb335a51f4cf22ff7fd1cfbf3aff1cf30
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2024-02-02 00:41:12 +0700

    publish

--- dist/_nuxt/static/1706807481/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706807481",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1330fc57"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null,"\u002F"));
