cedc0d36fd4dd3b2cd968bed853dbc9428834245
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2024-02-02 23:31:43 +0700

    add project

---
+++ dist/_nuxt/static/1706891420/blog/2023-07-22-ML2_ML_process/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706891420",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2023-07-22-ML2_ML_process",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7bfeaaa4"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null));
