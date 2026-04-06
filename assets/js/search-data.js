96c3aa4620485104ca73e0a1daf93e28a172282a
irfanmasoudi <irfanmasoudi@users.noreply.github.com>
2026-04-06 15:08:25 +0000

    Deploying to gh-pages from @ irfanmasoudi/irfanmasoudi.github.io@38519c626e5480e1766a8d2ac4dc92370208c37c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "My project collection summary.",
+          description: "Some projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
